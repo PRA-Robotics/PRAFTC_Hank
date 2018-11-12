931334de4ef53c464315e4e5d9e33ec6cdae73df
Noah M <43935662+noah-pm2001@users.noreply.github.com>
2018-11-12 16:50:11 -0700

    Reorganize
    Reorganized for clarity & easy expansion. See original for comments. Need to add Vuforia (currently w/o).

--- Com64_Autonomous.java
+++ Com64_Autonomous.java
@@ -1,118 +1,132 @@
 /**
-*Autonomous (Commodore 64)
-*Autonomous OpMode
-*Rookie Team -- Commodore 64
-*by Noah Mulvaney (NPM)
+*Autonomous OpMode for Commodore 64
+*PRA Robotics Rookie Team
+*Written by Noah Mulvaney
 **/
-/**
-To Do: check wheels & forward, determine base speed, test power-speed linearity, test aurisNav
-aurisNav -- simple test program for going foward 2 meters then stop and drop marker
-**/
+
 //import packages
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
-//automonous opmode
-@Autonomous(name = "Autonomous (Com64 11/2018 NPM)", group = "")
-public class Com64_Autonomous extends LinearOpMode {
-  //hardware variables declaration
-  private DcMotor leftMotor; //creates DcMotor instance for the left motor
-  private DcMotor rightMotor; //creates DcMotor instance for right motor
-  private Servo testServo; //creates Servo intance for the marker drop servo
- //overide ??
-  @Override
-	public void runOpMode() {
-		//general variables declarations
-		double correctionFactor; //factor for correcting differences in motor speed and power
-    boolean markerDropOpen; //tracks whether or not the marker drop servo is open
-    double servoPosition; //target position for servo
-    double leftPower; //sets left power from 0 to 10
-    double rightPower; //sets right power
-    boolean reverse; //state of reverse setting
-    double speedBase; //calulating settings for calulating other speeds
-    double rightSpeed; //calulated speed for right motor in centimeter per second
-    double leftSpeed; //calulated speed for left motor
-    double xPosition; //foward backward from start
-    double yPosition; //left right from start //not in use
-    double angle; //oriantation of robot //not in use
-    double time; //time in seconds
-    boolean stop; //variable for bringing robot to stop
-    int count;
-		//hardware variables initalization
-    leftMotor = hardwareMap.dcMotor.get("leftMotor");
-    rightMotor = hardwareMap.dcMotor.get("rightMotor");
-    testServo = hardwareMap.servo.get("testServo");
-    waitForStart(); //waits for start o be selected
+
+@Autonomous(name = "Autonomous NM", group = " ")
+
+void configure() {
+  correctionFactor = .85;
+  speedBase = 55 / 2;
+}
+
+void settings(double leftF, double rightF, boolean backF) {
+  leftPower = leftF;
+  rightPower = rightF;
+  reverse = backF;
+}
+
+void timeUpdate() {
+  time = count / 4;
+}
+
+void markerDrop() {
+  if (dropOpen) {
+    dropPosition = 1;
+  } else {
+    dropPosition = 0;
+  }
+}
+
+void reverse() {
+  if (reverse) {
+    if (leftPower > 0) {
+      leftPower = - leftPower;
+    }
+    if (rightPower > 0) {
+      rightPower = - rightPower;
+    }
+  }
+}
+
+void stop() {
+  if (stop) {
+    leftPower = 0;
+    rightPower = 0;
+  }
+}
+
+void speedUpdate() {
+  leftSpeed = leftPower * speedBase;
+  rightSpeed = rightPower * speedBase;
+}
+
+void hardwareUpdate() {
+  dropServo.setPosition(dropPosition);
+  leftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+  leftMotor.setPower(leftPower / 10);
+  rightMotor.setPower((rightPower * correctionFactor) / 10);
+}
+
+void telemetryData() {
+  telemetry.addData("X Position", xPosition + " cm");
+  /*
+  telemetry.addData("Y Position", yPosition + " cm");
+  telemetry.addData("Orientation", orientation + "°");
+  */
+  telemetry.addData("Time", time + " sec");
+  telemetry.addData("Drop Servo Position", dropPosition);
+  telemetry.addData("Left Motor Power", leftPower);
+  telemetry.addData("Right Motor Power", rightPower);
+  telemetry.addData("Left Motor Speed", leftSpeed + " cm/sec");
+  telemetry.addData("Right Motor Speed", rightSpeed + " cm/sec");
+}
+
+public class Autonomous extends LinearOpMode {
+  public void runOpMode() {
+    DcMotor leftMotor = hardwareMap.dcMotor.get("leftMotor");
+    DcMotor rightMotor = hardwareMap.dcMotor.get("rightMotor");
+    Servo dropServo = hardwareMap.dcMotor.get("testServo");
+    boolean stop = false;
+    boolean reverse = false;
+    boolean dropOpen = false;
+    int count = 0;
+    double time = 0;
+    double leftPower = 0;
+    double rightPower = 0;
+    double correctionFactor = 0;
+    double speedBase = 0;
+    double leftSpeed = 0;
+    double rightSpeed = 0;
+    double dropPosition = 0;
+    double xPosition = 0;
+    double yPosition = 0;
+    double orientation = 0;
+
+    waitForStart();
+
     if (opModeIsActive()) {
-      //non-control variable initalization
-      markerDropOpen = false; //sets marker drop door to closed
-      servoPosition = 0; //redundent initalization
-      stop = false; //start with unrestricted movement
-      xPosition = 0; //inital position
-      yPosition = 0; //inital position
-      angle = 0; //inital oriantation
-      time = 0; //initalization
-      speedBase = 55 / 2; //base speed setting
-      rightSpeed = 0; //initalization
-      leftSpeed = 0; //initalization
-      correctionFactor = .85; //correction factor setting
-      count = 0; //initalization
-      //inital control variable initalization
-      leftPower = 2; //inital power
-      rightPower = 2; //inital power
-      reverse = false; //inital reverse setting
-	    while (opModeIsActive()) {
-        //aurisNav
-        xPosition = leftSpeed * time; //position based on set speed
-        if (xPosition > 200) { //after robot passes two meters
-          stop = true; //stop robot
-          markerDropOpen = true; //drop marker
+      configure();
+      settings(2, 2, false);
+
+      while (opModeIsActive()) {
+        timeUpdate();
+
+        xPosition = leftSpeed * time;
+        if (xPosition > 200) {
+          stop = true;
+          dropOpen = true;
         }
-				//general control and variable updates
-	      if (markerDropOpen) { //if drop door should be open
-	        servoPosition = 1; //open
-	      } else {
-	        servoPosition = 0; //close
-	      }
-        if (reverse) { //reverse setting
-          if (leftPower > 0) { //if left motor is forward
-            leftPower = - leftPower; //make backwards
-          }
-          if (rightPower > 0) { //if right motor is forward
-            rightPower = - rightPower; //make backwards
-          }
-        }
-        if (stop) { //stop setting
-          leftPower = 0; //cut power
-          rightPower = 0;
-        }
-        time = count / 4;
-        leftSpeed = leftPower * speedBase; //speed calculation
-        rightSpeed = rightPower * speedBase;
-				//updating hardware
-        testServo.setPosition(servoPosition);
-		    leftMotor.setDirection(DcMotorSimple.Direction.REVERSE); //reverse so foward is forward
-		    leftMotor.setPower(leftPower / 10); //converts power to between 0 and 1
-		    rightMotor.setPower((rightPower * correctionFactor) / 10); //accounts for correction and converts to range
-				//telemetry data
-        telemetry.addData("X Position", xPosition); //robot position
-        /*telemetry.addData("Y Position", yPosition); */ //not in use
-        /*telemetry.addData("Orientation", angle); */ //robot orientation //not in use
-        telemetry.addData("Time", time); //system time in seconds
-        telemetry.addData("Servo Position", servoPosition); //servo position
-		    telemetry.addData("Left Motor Power", leftPower); //power from 0 to 10
-        /*telemetry.addData("Left Motor Speed", leftSpeed); */ //calculated speed in centimeters per second //not in use
-		    telemetry.addData("Right Motor Power", rightPower);
-        /*telemetry.addData("Right Motor Speed", rightSpeed); */ //not in use
-        telemetry.addData("Reverse Setting", reverse); //reverse setting
- 				telemetry.addData("Correction Factor", correctionFactor); //correction factor //calabrate and remove
-        //update telemetry
-		    telemetry.update();
-        //count & timing
+
+        markerDrop();
+        reverse();
+        stop();
+        speedUpdate();
+
+        hardwareUpdate();
+        telemetryData();
+        telemetry.Update();
+
         sleep(250);
         count ++;
-    	}
-  	}
-	}
+      }
+    }
+  }
 }
