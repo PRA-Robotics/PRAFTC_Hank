38dc8069c26f114821b01a22fff94e4763f34250
Noah M <43935662+noah-pm2001@users.noreply.github.com>
2018-11-13 12:33:53 -0700

    Add comments
    Autonomous program organized with functions, containing full comments, without Vuforia. May contain bugs, has not been built.

--- Autonomous.java
+++ Autonomous.java
@@ -3,91 +3,108 @@
 *PRA Robotics Rookie Team
 *Written by Noah Mulvaney
 **/
-
 //import packages
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
-
+//set opMode and program name displayed on FTC app
 @Autonomous(name = "Autonomous NM", group = " ")
-
+//for configuring static varaiables
 void configure() {
-  correctionFactor = .85;
-  speedBase = 55 / 2;
+  correctionFactor = .85; //adjustment to rightMotor power to straighen course, determined expirementally
+  speedBase = 55 / 2; //speed in centimeters per second, determined expirementally
+  interval = 4; //time accuaracy for wait, count, and time system, a larger value results in less error from run time but reduces rate of commanded changes
 }
-
-void settings(double leftF, double rightF, boolean backF) {
-  leftPower = leftF;
-  rightPower = rightF;
-  reverse = backF;
+//for setting intial motor power
+void settings(double leftF, double rightF, int intervalF) {
+  leftPower = leftF; //inital left power
+  rightPower = rightF; //inital right power
 }
-
+//updates time and adds count
 void timeUpdate() {
-  time = count / 4;
+  time = count / interval; //calculate time in seconds
+  count ++; //add loop count
 }
-
-void markerDrop() {
-  if (dropOpen) {
-    dropPosition = 1;
-  } else {
+//lower door to drop marker
+void dropMarker() {
+  dropPosition = 1; //full open
+}
+//close door on marker drop
+void closeDrop() {
+  dropPosition = 0; //closed
+}
+//reverse both motors
+void reverse() {
+  if (leftPower > 0) { //if left power is foward
+    leftPower = - leftPower; //make backwards
+  }
+  if (rightPower > 0) { //if right power is foward
+    rightPower = - rightPower; //make backwards
+  }
+}
+//stop robot by cutting power
+void stop() {
+  leftPower = 0; //brake left motor
+  rightPower = 0; //brake right motor
+}
+//recalculates speed values
+void speedUpdate() {
+  leftSpeed = leftPower * speedBase; //left speed
+  rightSpeed = rightPower * speedBase; //right speed
+}
+//variables to harware update
+void hardwareUpdate() {
+  dropServo.setPosition(dropPosition); //marker drop servo position
+  leftMotor.setDirection(DcMotorSimple.Direction.REVERSE); //reverse left motor to make positive forward
+  leftMotor.setPower(leftPower / 10); //left motor adjusted for correct range
+  rightMotor.setPower((rightPower * correctionFactor) / 10); //right motor adjusted for straightness and correct range
+}
+//variables to telemetry data
+void telemetryData() {
+  telemetry.addData("X Position", xPosition + " cm"); //forward backwards position on field
+  telemetry.addData("Y Position", yPosition + " cm"); //left right from start position
+  telemetry.addData("Orientation", orientation + "°"); //orientation where start is 0°
+  telemetry.addData("Time", time + " sec"); //time in seconds
+  telemetry.addData("Drop Servo Position", dropPosition); //position of servo on marker drop
+  telemetry.addData("Left Motor Power", leftPower); //left motor power
+  telemetry.addData("Right Motor Power", rightPower); //right motor power
+  telemetry.addData("Left Motor Speed", leftSpeed + " cm/sec"); //calculated left speed
+  telemetry.addData("Right Motor Speed", rightSpeed + " cm/sec"); //calcualated right speed
+}
+//chack variables for correct range
+void variableCheck() {
+  //drop position between 0 and 1
+  if (dropPosition < 0) {
     dropPosition = 0;
   }
-}
-
-void reverse() {
-  if (reverse) {
-    if (leftPower > 0) {
-      leftPower = - leftPower;
-    }
-    if (rightPower > 0) {
-      rightPower = - rightPower;
-    }
+  if (dropPosition > 1) {
+    dropPosition = 1;
+  }
+  //power between -10 and 10
+  if (leftPower < -10) {
+    leftPower = -10;
+  }
+  if (leftPower > 10) {
+    leftPower = 10;
+  }
+  if (rightPower < -10) {
+    rightPower = -10;
+  }
+  if (rightPower > 10) {
+    rightPower = 10;
   }
 }
-
-void stop() {
-  if (stop) {
-    leftPower = 0;
-    rightPower = 0;
-  }
-}
-
-void speedUpdate() {
-  leftSpeed = leftPower * speedBase;
-  rightSpeed = rightPower * speedBase;
-}
-
-void hardwareUpdate() {
-  dropServo.setPosition(dropPosition);
-  leftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-  leftMotor.setPower(leftPower / 10);
-  rightMotor.setPower((rightPower * correctionFactor) / 10);
-}
-
-void telemetryData() {
-  telemetry.addData("X Position", xPosition + " cm");
-  /*
-  telemetry.addData("Y Position", yPosition + " cm");
-  telemetry.addData("Orientation", orientation + "°");
-  */
-  telemetry.addData("Time", time + " sec");
-  telemetry.addData("Drop Servo Position", dropPosition);
-  telemetry.addData("Left Motor Power", leftPower);
-  telemetry.addData("Right Motor Power", rightPower);
-  telemetry.addData("Left Motor Speed", leftSpeed + " cm/sec");
-  telemetry.addData("Right Motor Speed", rightSpeed + " cm/sec");
-}
-
+//primary class constructor
 public class Autonomous extends LinearOpMode {
+  //when program is selected
   public void runOpMode() {
+    //declare and initalize variables
     DcMotor leftMotor = hardwareMap.dcMotor.get("leftMotor");
     DcMotor rightMotor = hardwareMap.dcMotor.get("rightMotor");
     Servo dropServo = hardwareMap.dcMotor.get("testServo");
-    boolean stop = false;
-    boolean reverse = false;
-    boolean dropOpen = false;
     int count = 0;
+    int interval = 0;
     double time = 0;
     double leftPower = 0;
     double rightPower = 0;
@@ -99,33 +116,34 @@ public class Autonomous extends LinearOpMode {
     double xPosition = 0;
     double yPosition = 0;
     double orientation = 0;
-
-    waitForStart();
-
-    if (opModeIsActive()) {
+    waitForStart(); //wait for start button to be pressed
+    if (opModeIsActive()) { //when running
+      //inital servo and motor positions
+      closeDrop();
+      stop();
+      //configurations and settings
       configure();
-      settings(2, 2, false);
-
-      while (opModeIsActive()) {
+      settings(2, 2);
+      while (opModeIsActive()) { //loop while running
+        //updated time
         timeUpdate();
-
-        xPosition = leftSpeed * time;
-        if (xPosition > 200) {
-          stop = true;
-          dropOpen = true;
+        //test program
+        xPosition = leftSpeed * time; //calculate position for linear motion at constant speed
+        if (xPosition > 200) { //after two meters
+          dropMarker(); //drop marker
+          stop(); //stop moving
+          wait(2000); //wait two seconds
+          closeDrop(); //close drop door
         }
-
-        markerDrop();
-        reverse();
-        stop();
+        //update variables
         speedUpdate();
-
+        variableCheck();
+        //update hardware and telemetry
         hardwareUpdate();
         telemetryData();
         telemetry.Update();
-
-        sleep(250);
-        count ++;
+        //sleep
+        sleep(1000 / interval);
       }
     }
   }
