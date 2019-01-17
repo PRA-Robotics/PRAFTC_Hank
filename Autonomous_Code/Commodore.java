5f921680f07b925d72b59f0d44782040eae1a4f4
Noah M <nooahpmulvaney@gmail.com>
2019-01-16 20:47:14 -0700

    Update flow

--- Autonomous_Code/Commodore.java
+++ Autonomous_Code/Commodore.java
@@ -8,7 +8,7 @@ import org.firstinspires.ftc.robotcore.external.JavaUtil;
 public class Commodore extends LinearOpMode {
   Vision vision = new Vision();
   Hardware robot = new Hardware();
-  Config config = new Config(.2, 16.505, 6); //.7 lift, .4 tilt
+  Config config = new Config(.2, 16.505, 6);
 
   public void runOpMode() {
     vision.init();
@@ -18,18 +18,25 @@ public class Commodore extends LinearOpMode {
     if (opModeIsActive()) {
       vision.open();
       robot.motorStart(config);
+
       robot.servo(0, .3, .5, 0);
+      //sleep();
+      robot.servo(0, .7, .5, 0);
+      //sleep();
+      robot.servo(0, .7, .4, 0);
+      //sleep();
+      robot.servo(0, .3, .4, 0);
+      //sleep();
+      robot.servo(0, .3, .5, 0);
+      robot.turn(config, -45);
+      robot.forward(config, 250);
+      robot.turn(config, 90);
+      robot.forward(config, 200);
+      robot.servo(.8, .3, .5, 0);
 
-      int loop = 0;
-
-      while(opModeIsActive()) {
-        if (loop == 0) {
-          robot.turn(config, 90);
-        }
-
-        loop++;
-      }
       vision.shut();
     }
   }
 }
+
+//https://ftctechnh.github.io/ftc_app/doc/javadoc/index.html
